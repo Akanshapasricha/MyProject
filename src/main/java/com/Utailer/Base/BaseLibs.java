411d1310a2ec602e4d2a9c05f42497221bc00278
Akansha <Akansha@Source-4443.sourcesoftsolutions.com>
2019-06-26 15:12:39 +0530

    test

--- src/main/java/com/Utailer/Base/BaseLibs.java
+++ src/main/java/com/Utailer/Base/BaseLibs.java
@@ -1,6 +1,4 @@
 package com.Utailer.Base;
-
-
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -8,6 +6,7 @@ import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.events.EventFiringWebDriver;
 
 import com.Utailer.Utility.UtilsTest;
@@ -20,8 +19,8 @@ public class BaseLibs {
 	{
 		try 
 		{
-						prop=new Properties();
-			FileInputStream fileInputStream=new FileInputStream("C:\\Users\\akansha\\eclipse-workspace\\WebProject\\src\\main\\java\\com\\Utailer\\Config\\config.properties");
+			prop=new Properties();
+			FileInputStream fileInputStream=new FileInputStream("C:\\Users\\akansha\\git\\WebProject\\WebProject\\src\\main\\java\\com\\Utailer\\Config\\config.properties");
 			prop.load(fileInputStream);
 		} 
 		catch (FileNotFoundException e) {
@@ -36,10 +35,23 @@ public class BaseLibs {
 	public static void initialization() {
 		String browserName=prop.getProperty("browser");
 
-		if(browserName.equals("chrome")) 
-		{
-			System.setProperty("webdriver.chrome.driver", "C:\\Users\\akansha\\eclipse-workspace\\WebProject\\exeFiles\\chromedriver.exe");
-			driver=new ChromeDriver();
+		//		if(browserName.equals("chrome")) 
+		//		{
+		//			System.setProperty("webdriver.chrome.driver", "C:\\Users\\akansha\\eclipse-workspace\\WebProject\\exeFiles\\chromedriver.exe");
+		//			driver=new ChromeDriver();
+		//			e_driver = new EventFiringWebDriver(driver);
+		//			// Now create object of EventListerHandler to register it with EventFiringWebDriver
+		//			driver = e_driver;
+		//			driver.manage().window().maximize();
+		//			driver.manage().window().maximize();
+		//			driver.manage().deleteAllCookies();
+		//			driver.manage().timeouts().pageLoadTimeout(UtilsTest.PAGE_LOAD_TIMEOUT, TimeUnit.SECONDS);
+		//			driver.manage().timeouts().implicitlyWait(UtilsTest.IMPLICIT_WAIT, TimeUnit.SECONDS);
+		//			driver.get(prop.getProperty("url"));
+		//		}
+		if (browserName.equals("ff")) {
+			System.setProperty("webdriver.gecko.driver", "C:\\Users\\akansha\\git\\WebProject\\WebProject\\exeFiles\\geckodriver.exe");
+			driver=new FirefoxDriver();
 			e_driver = new EventFiringWebDriver(driver);
 			// Now create object of EventListerHandler to register it with EventFiringWebDriver
 			driver = e_driver;
